ed5b33eb81c324639368d73be346104537f2182d
anonymous anonymous <sarah.rogers@kellogg.northwestern.edu>
2022-02-24 08:05:47 +0000

    Whitelist columns in User for ActiveAdmin

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register User do
 
+ permit_params :username, :email, :password, :posts_count, :followers_count, :following_count
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
